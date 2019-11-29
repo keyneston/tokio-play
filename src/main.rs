21a1736f5cb2dcf5c6cbd74b0e16783853630336
Tabitha Rollins <tabitha@keyneston.com>
2019-11-29 16:36:42 +0000

    Print stuff to stdout

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+use chrono::{Datelike, Timelike, Utc};
 use futures::StreamExt;
+//use std::io::{self, Write};
 use std::time::{Duration, SystemTime};
 use tokio::io;
 use tokio::net::{TcpListener, TcpStream};
@@ -15,6 +17,8 @@ async fn main() {
     let server = async move {
         let mut incoming = listener.incoming();
 
+        // Wait for connections. On receiving a connection spawn it off into a new "green thread"
+        // and run the handler.
         while let Some(socket_res) = incoming.next().await {
             match socket_res {
                 Ok(socket) => {
@@ -33,18 +37,39 @@ async fn main() {
     server.await;
 }
 
-async fn handler(socket: TcpStream) {
+async fn handler(mut socket: TcpStream) {
     let t = Duration::new(2, 0);
 
-    println!(
-        "Accepted connection from {:?} at {1:?}",
+    let buf = log_entry(&format!(
+        "Accepted connection from {:?}",
         socket.peer_addr(),
-        SystemTime::now(),
-    );
+    ));
+
+    let mut buf = [0; 1024];
+    match socket.read(&mut buf).await {
+        // socket closed
+        Ok(n) => {
+            io::stdout().write_all(&buf[0..n]).await;
+        }
+        Err(e) => {
+            println!("failed to read from socket; err = {:?}", e);
+            return;
+        }
+    };
     delay_for(t).await;
-    println!(
-        "Finished connection from {:?} at {1:?}",
-        socket.peer_addr(),
-        SystemTime::now(),
-    );
+
+    log_entry(&format!(
+        "Finished connection from {:?}",
+        socket.peer_addr()
+    ))
+}
+
+fn log_entry(msg: &str) {
+    println!("{} -- {}", fmt_date(), msg)
+}
+
+fn fmt_date() -> String {
+    let now = Utc::now();
+
+    now.to_rfc3339()
 }
